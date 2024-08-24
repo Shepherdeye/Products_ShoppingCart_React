9bb6f1ecf7e382540b88ca69bf6ee11871d233e9
Shepherd's Eye <134965007+Shepherdeye@users.noreply.github.com>
2024-08-24 21:24:16 +0200

    Deploy

--- src/Components/Data/CurrencyFormater.js
+++
@@ -1,8 +0,0 @@
-const CURRENCY_FORMATER = new Intl.NumberFormat(undefined, {
-    currency: "USD",
-    style: "currency"
-})
-const FormatCurrency = (num) => {
-    return CURRENCY_FORMATER.format(num)
-}
-export default FormatCurrency
